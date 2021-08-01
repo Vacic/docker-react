ccf82ecfb126466489bf8205316cd27dc63abed2
vacic <nikolavacic@ymail.com>
2021-08-01 16:27:18 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>New Text. tex textf wefwefwebjhb</p>
+        <p>New Text.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
