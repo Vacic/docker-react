c79bbd0eca8f02af1f0b62a84ebb3ff41b4bcf07
vacic <nikolavacic@ymail.com>
2021-08-01 23:27:44 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>New Text. NEEEW</p>
+        <p>New Text. Feature Branch yay!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
